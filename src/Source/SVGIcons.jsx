3fa47d82c0d3b141d243589316bd9fd9696eeb17
Aminjon <vafokulov552@gmail.com>
2022-10-08 14:40:24 +0500

    new side create next js

---
+++ src/Source/SVGIcons.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SVGIcons = () => {
+    switch (icon) {
+        case value:
+            // return()
+            break;
+        case logo:
+            // return()
+        default:
+            break;
+    }
+}
+
+export default SVGIcons
