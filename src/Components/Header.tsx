de857c5c1fceafddca8029530222af648a99dc45
Aminjon <vafokulov552@gmail.com>
2022-10-11 17:41:46 +0500

    push

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Box, Text } from '@chakra-ui/react';
+import Instagram from '../../public/img/Instagram.png'
 
 type Props = {}
 
 const Header = (props: Props) => {
+  const Image: string = Instagram.src
   return (
-    <div>Header</div>
+    <div className='header'>
+      <Box className='head_left' w={'15%'} height={'100%'}>
+        <Text>контактная информация</Text>
+        <Box w={'90px'} h={'2px'} bgColor={'white'}></Box>
+      </Box>
+      <img src={Image} alt="" />
+    </div>
   )
 }
 
