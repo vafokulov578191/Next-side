cacdc09bf22738d6684671c8ad92fa335fa39fba
Aminjon <vafokulov552@gmail.com>
2022-10-14 16:37:36 +0500

    created: media 768px and ready

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -10,11 +10,11 @@ const Footer = (props: Props) => {
 
   return (
     <div className='footer'>
-      <Box w={{ xl: '1034px', lg: '1034px', md: '100%' }} h={'440px'} display={'flex'} pl={{ xl: '0px', lg: '0px', md: '35px' }} flexDirection={'column'} margin={'0px auto'} marginBottom={'120px'} textAlign={'center'} color={'white'}>
+      <Box w={{ xl: '1034px', lg: '1034px', md: '100%' }} h={'440px'} display={'flex'} pl={{ xl: '0px', lg: '0px', md: '35px' }} flexDirection={'column'} margin={'0px auto'} marginBottom={{ xl: '120px', lg: '120px', md: '120px', sm: '90px' }} textAlign={'center'} color={'white'}>
         <h1 style={{ fontSize: '50px', fontWeight: '300', textTransform: 'uppercase', marginBottom: '100px' }}><span style={{ color: '#D1A954' }}>партнеры</span> театра</h1>
-        <Box display={'grid'} gridTemplateColumns={'repeat(4, 1fr)'} gap={{ xl: '80px', lg: '80px', md: '30px' }} >
+        <Box display={'grid'} gridTemplateColumns={'repeat(4, 1fr)'} gap={{ xl: '80px', lg: '80px', md: '30px' }} gridGap={{ xl: '0px', lg: '0px', md: '0px', sm: '20px' }} >
           {
-            [1, 2, 3, 4, 5, 6, 7, 8].map(item => <Image key={item} src={Gas} alt='alt' width={{ xl: '200px', lg: '200px', md: '150px' }}
+            [1, 2, 3, 4, 5, 6, 7, 8].map(item => <Image key={item} src={Gas} alt='alt' width={{ xl: '200px', lg: '200px', md: '150px', sm: '150px' }}
               height={'96px'} objectFit={'contain'} />)
           }
         </Box>
@@ -25,7 +25,7 @@ const Footer = (props: Props) => {
       <Box w={{ xl: '1110px', lg: '1110px', md: '690px' }} h={'360px'} display={'flex'} flexDirection={'column'} margin={'0px auto'} marginBottom={'80px'} gap={'100px'} textAlign={'center'} color={'white'} fontWeight={'300'}>
         <h1 style={{ fontSize: '50px', textTransform: 'uppercase' }}><span style={{ color: '#D1A954' }}>стать</span> меценатом</h1>
         <Box w={'100%'} height={'100%'} display={'flex'} alignItems={'center'} gap={'100px'}>
-          <Flex display={{xl: 'flex', lg: 'flex', md: 'none'}} direction={'column'} w={'400px'} alignItems={'flex-start'} gap={'25px'} fontWeight={'700'}>
+          <Flex display={{ xl: 'flex', lg: 'flex', md: 'none', sm: 'none' }} direction={'column'} w={'400px'} alignItems={'flex-start'} gap={'25px'} fontWeight={'700'}>
             <Text>+7 (812) 123-45-55</Text>
             <Text>+7 (812) 123-45-66</Text>
             <Text>info@theater.ru</Text>
@@ -34,7 +34,7 @@ const Footer = (props: Props) => {
 
           <form style={{ height: '100%', width: '100%', display: 'flex', flexDirection: 'column', gap: '40px', alignItems: 'flex-end', justifyContent: 'flex-end' }}>
 
-            <Box w={'100%'} display={'flex'} gap={{xl: '0px', lg: '0px', md: '20px'}}>
+            <Box w={'100%'} display={'flex'} gap={{ xl: '0px', lg: '0px', md: '20px' }}>
               <Flex w={'220px'} direction={'column'} gap={'30px'}>
                 <Input pl={'15px'} w={'200px'} h={'35px'} placeholder='Имя' />
                 <Input pl={'15px'} w={'200px'} h={'35px'} placeholder='Телефон' />
@@ -57,7 +57,7 @@ const Footer = (props: Props) => {
 
       {/* Forms */}
 
-      <Box w={{xl: '1110px', lg: '1110px', md: '700px'}} h={'50px'} display={'flex'} flexDirection={'column'} margin={'0px auto'} marginBottom={'30px'}>
+      <Box w={{ xl: '1110px', lg: '1110px', md: '700px', sm: '700px' }} h={'50px'} display={'flex'} flexDirection={'column'} margin={'0px auto'} marginBottom={'30px'}>
         <Flex w={'100%'} h={'2px'} bgColor={'#565656'}></Flex>
         <Box color={'#989898'} display={'flex'} w={'100%'} h={'100%'} alignItems={'flex-end'} justifyContent={'space-between'}>
           <Text>(с) Все права защищены. Большой театр. 2020</Text>
--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -15,15 +15,15 @@ const Main = (props: Props) => {
 
   return (
     <div className='Main'>
-      <Box w={{ xl: '1130px', lg: '1130px', md: '1130px' }} h={{ xl: '480px', lg: '480px', md: '100%' }} margin={'150px auto'} display={'flex'} flexDirection={'column'} gap={'100px'} textAlign={'center'}>
+      <Box w={{ xl: '1130px', lg: '1130px', md: '1130px', sm: '750px' }} h={{ xl: '480px', lg: '480px', md: '100%', sm: '100%' }} margin={'150px auto'} display={'flex'} flexDirection={'column'} gap={'100px'} textAlign={'center'}>
         <h1 style={{ textTransform: 'uppercase', fontSize: '50px', color: 'white' }}><span style={{ color: '#D1A954' }}>о</span> театре</h1>
-        <Box w={'100%'} height={'100%'} position={'relative'} left={'200px'} display={'flex'} gap={'40px'} margin={{ xl: '0px', lg: '0px', md: '0px auto' }}>
+        <Box w={'100%'} height={'100%'} position={'relative'} left={{ xl: '200px', lg: '200px', md: '200px', sm: '0px' }} display={'flex'} gap={'40px'} margin={{ xl: '0px', lg: '0px', md: '0px auto', sm: '0px auto' }}>
           <Stack direction={'column'} textAlign={'start'} w={'450px'} gap={'30px'} color={'white'}>
             <Text variant={'b'} fontWeight={'700'} fontSize={'22px'} textTransform={'uppercase'}>Самый лучший театр СПБ</Text>
             <Text>Как свидетельствуют архивные документы, первое каменное здание Большого театра начали возводить в 1775 году по проекту Антонио Ринальди. В дальнейшем, после того как Ринальди упал с лесов и не мог лично наблюдать за ходом работ, Екатерина II поручила немецкому театральному декоратору и архитектору Людвигу Филиппу Тишбейну создать новый проект театра, который и был воплощён архитекторами Ф. В. фон Бауром и М. А. Деденёвым. Открытие его состоялось в 1783 году, хотя, основываясь на других свидетельствах, годом открытия можно считать 1784 год.</Text>
           </Stack>
 
-          <Stack direction={'column'} w={'fit-content'} color={'white'} gap={'30px'} alignItems={{ xl: 'baseline', lg: 'baseline', md: 'flex-start' }}>
+          <Stack direction={'column'} w={'fit-content'} color={'white'} gap={'30px'} alignItems={{ xl: 'baseline', lg: 'baseline', md: 'flex-start', sm: 'flex-start' }}>
             <Text fontWeight={'700'} fontSize={'22px'} textTransform={'uppercase'}>немного о цифрах</Text>
             <Box display={'flex'} flexDirection={'column'} gap={'20px'} fontWeight={'700'}>
               <Image src={Icon} alt="Icon" width={'70px'} height={'70px'} objectFit={'contain'} />
@@ -41,9 +41,9 @@ const Main = (props: Props) => {
 
       {/* last block */}
 
-      <Box w={{ xl: '1730px', lg: '1730px', md: '1000px' }} height={'715px'} display={'flex'} flexDirection={'column'} color={'white'} position={'relative'} left={'200px'} marginBottom={'150px'}>
+      <Box w={{ xl: '1730px', lg: '1730px', md: '1000px' }} height={'715px'} display={'flex'} flexDirection={'column'} color={'white'} position={'relative'} left={{ xl: '200px', lg: '200px', md: '200px', sm: '0px' }} marginBottom={'150px'}>
         <h1 style={{ margin: '0px auto', fontSize: '50px', fontWeight: '300', marginBottom: '100px' }}><span style={{ color: '#D1A954' }}>наши</span> постановки</h1>
-        <Box display={'grid'} w={{xl: '100%', lg: '100%', md: '880px'}} gridTemplateColumns={{ xl: 'repeat(4, 1fr)', lg: 'repeat(4, 1fr)', md: 'repeat(2, 1fr)' }} h={'550px !important'}>
+        <Box display={'grid'} w={{ xl: '100%', lg: '100%', md: '880px', sm: '100%' }} gridTemplateColumns={{ xl: 'repeat(4, 1fr)', lg: 'repeat(4, 1fr)', md: 'repeat(2, 1fr)', sm: 'repeat(2, 1fr)' }} h={'550px !important'} >
           <Flex className='bg' flexDirection={'column'} justifyContent={'space-between'} p={'40px'} textTransform={'uppercase'} overflow={'hidden'} position={'relative'} fontWeight={'700'} >
             <Text>ОСКАР 2020</Text>
             <Text position={'absolute'} fontWeight={'700'} bottom={'85px'} left={'-10px'} transform={'rotate(-90deg)'}>сентябрь 2020</Text>
@@ -53,11 +53,11 @@ const Main = (props: Props) => {
               <Search2Icon w={'30px'} h={'30px'} />
             </Button>
           </Flex>
-          <Flex className='bg3' display={{ xl: 'flex', lg: 'flex', md: 'none' }} p={'40px'} flexDirection={'column'} justifyContent={'space-between'} textTransform={'uppercase'} overflow={'hidden'} position={'relative'} fontWeight={'700'} >
+          <Flex className='bg3' display={{ xl: 'flex', lg: 'flex', md: 'none', sm: 'none' }} p={'40px'} flexDirection={'column'} justifyContent={'space-between'} textTransform={'uppercase'} overflow={'hidden'} position={'relative'} fontWeight={'700'} >
             <Text>золотой грамофон</Text>
             <Text position={'absolute'} fontWeight={'700'} bottom={'85px'} left={'-10px'} transform={'rotate(-90deg)'}>июль 2020</Text>
           </Flex>
-          <Flex className='bg4' display={{ xl: 'flex', lg: 'flex', md: 'none' }} p={'40px'} flexDirection={'column'} justifyContent={'space-between'} textTransform={'uppercase'} overflow={'hidden'} position={'relative'} fontWeight={'700'} >
+          <Flex className='bg4' display={{ xl: 'flex', lg: 'flex', md: 'none', sm: 'none' }} p={'40px'} flexDirection={'column'} justifyContent={'space-between'} textTransform={'uppercase'} overflow={'hidden'} position={'relative'} fontWeight={'700'} >
             <Text>щелкунчик</Text>
             <Text position={'absolute'} fontWeight={'700'} bottom={'85px'} left={'-10px'} transform={'rotate(-90deg)'}>май 2020</Text>
           </Flex>
@@ -66,7 +66,7 @@ const Main = (props: Props) => {
 
       {/* Postanovki */}
 
-      <Box className='rating' w={{ xl: '1110px', lg: '1110px', md: '100%' }} h={'550px'} margin={'0px auto'} marginBottom={'150px'} display={'flex'} alignItems={'center'} justifyContent={'center'}>
+      <Box className='rating' w={{ xl: '1110px', lg: '1110px', md: '100%', sm: '100%' }} h={'550px'} padding={{ xl: '0px', lg: '0px', md: '0px', sm: '60px' }} margin={'0px auto'} marginBottom={'150px'} display={'flex'} alignItems={'center'} justifyContent={'center'}>
         <Box w={'750px'} h={'300px'} display={'flex'} flexDirection={'column'} color={'#D1A954'} fontWeight={'700'}>
           <Flex gap={'130px'} alignItems={'center'}>
             <Text fontSize={'70px'}>2018</Text>
@@ -78,11 +78,11 @@ const Main = (props: Props) => {
           </Flex>
           <Flex gap={'130px'} alignItems={'center'}>
             <Text fontSize={'70px'}>2019</Text>
-            <Text fontSize={'34px'} color={'white'}>Лучший театр</Text>
+            <Text fontSize={{ xl: '34px', lg: '34px', md: '34px', sm: '29px' }} color={'white'}>Лучший театр</Text>
           </Flex>
           <Flex gap={'130px'} alignItems={'center'}>
             <Text fontSize={'70px'}>2020</Text>
-            <Text fontSize={'34px'} color={'white'}>по мнению театральных критиков Парижа</Text>
+            <Text fontSize={{ xl: '34px', lg: '34px', md: '34px', sm: '29px' }} color={'white'}>по мнению театральных критиков Парижа</Text>
           </Flex>
         </Box>
       </Box>
